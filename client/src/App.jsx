7c0bc2b95cdeb93ad8a620ca7ee75050a7897ca3
vishesh wasnik <visheshs.wasnik@thewitslab.com>
2024-06-26 15:40:15 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import About from './pages/About'
 import SignInn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
